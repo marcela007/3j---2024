fcdcdf022bbde0ecad07744e3a99afff88c7e9f5
marcela007 <110061268+marcela007@users.noreply.github.com>
2024-06-14 09:42:30 -0300

    14.06

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
+tempo[0].textContent= "banana"
+const tempoObjetivo1 = new Date(2024,5,3,15,4,34);
+const tempoObjetivo2 = new Date(2024,5,3,15,4,34);
+const tempoObjetivo3 = new Date(2024,5,3,15,4,34);
+const tempoObjetivo4 = new Date(2024,5,3,15,4,34);
+const agora = new Date();
+tempo[1].textContent = agora
 for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
         for (let j = 0 ; j < botoes.length ; j++){
