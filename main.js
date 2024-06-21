1360b6b67a78101e543f8023539be36e03c879f7
marcela007 <110061268+marcela007@users.noreply.github.com>
2024-06-21 09:45:07 -0300

    21-06

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
-tempo[0].textContent= "banana"
-const tempoObjetivo1 = new Date(2024,7,3,15,4,34);
-const tempoObjetivo2 = new Date(2024,7,3,15,4,34);
-const tempoObjetivo3 = new Date(2024,7,3,15,4,34);
-const tempoObjetivo4 = new Date(2024,7,3,15,4,34);
+tempo[0].textContent = "Bibs"
+const tempoObjetivo1 = new Date(2024,11,31,59,59);
+const tempoObjetivo2 = new Date(2024,11,31,59,59);
+const tempoObjetivo3 = new Date(2024,11,31,59,59);
+const tempoObjetivo4 = new Date(2024,11,31,59,59);
 const agora = new Date();
 let segundos;
-let minutos;
+let minuto;
 let horas;
 let dias;
-segundos= (tempoObjetivo1-agora)/100
-minutos= segundos/60
-horas= minutos/60
-dias= horas/24
+segundos=(tempoObjetivo1-agora)/1000
+minutos=segundos/60
+horas=minutos/60
+dias=horas/24
+segundos=Math.floor(segundos);
+minutos=Math.floor(minutos);
+horas=Math.floor(horas);
+dias=Math.floor(dias);
+tempo[0].textContent=`Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
 tempo[1].textContent = agora
-tempo[0].textContent = tempoObjetivo1-agora
 
 for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
